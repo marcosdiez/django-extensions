07d3d38e797065659a69bccc58adc4ae9214df53
Lucas Taylor <ltaylor.volks@gmail.com>
2012-04-20 17:54:07 -0700

    Fix failure in python 2.6 when using "shell_plus --print-sql"
    Change introduced in [15c81cd] to display sql execution time relies on a method of datetime.timedelta introduced in python 2.7 (total_seconds). Using time.time() is simpler and backwards compatible.

--- django_extensions/management/commands/shell_plus.py
+++ django_extensions/management/commands/shell_plus.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
+
 import os
 from django.core.management.base import NoArgsCommand
 from optparse import make_option
-from time import time
 
 
 class Command(NoArgsCommand):
@@ -42,18 +43,14 @@ class Command(NoArgsCommand):
 
             class PrintQueryWrapper(util.CursorDebugWrapper):
                 def execute(self, sql, params=()):
-                    starttime = time()
                     try:
                         return self.cursor.execute(sql, params)
                     finally:
                         raw_sql = self.db.ops.last_executed_query(self.cursor, sql, params)
-                        execution_time = time() - starttime
                         if sqlparse:
                             print sqlparse.format(raw_sql, reindent=True)
                         else:
                             print raw_sql
-                        print
-                        print 'Execution time: %.6fs' % execution_time
                         print
 
             util.CursorDebugWrapper = PrintQueryWrapper
